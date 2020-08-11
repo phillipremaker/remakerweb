0eca5ccd282e27afaf12b5fa74fa7a2ffb9ab9fb
Valerie Remaker <44219004+vremaker@users.noreply.github.com>
2020-08-10 17:19:00 -0700

    navbar toggle works

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+import Navvy from "./navvy";
+function App() {
+  return (
+    <div className="App">
+    <Navvy />
+    </div>
+  );
+}
+
+export default App;
